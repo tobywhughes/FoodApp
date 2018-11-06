7a9c5a5fbbeb37d882f8e88afbcec799ef9e9ac9
Toby Hughes <tobywhughes@gmail.com>
2018-11-05 17:47:23 -0800

    Rest of query functions

--- Api/Database/db_query.py
+++ Api/Database/db_query.py
@@ -13,26 +13,83 @@ def query_restaurant(id, connection, cursor):
     return create_return_dict(cursor)
 
 @db_connect
-def query_restaurant(id, connection, cursor):
-    pass
-
- @db_connect
 def query_menu(id, rid, connection, cursor):
-    pass
+    if id is None and rid is None:
+        execute_string = build_select('menu_item', [])
+        cursor.execute(execute_string)
+    else:
+        n_ = []
+        t_ = []
+        if id is not None:
+            n_.append('id')
+            t_.append(id)
+        if rid is not None:
+            n_.append('rid')
+            t_.append(rid)
+        execute_string = build_select('menu_item', n_)
+        cursor.execute(execute_string, t_)
+    return create_return_dict(cursor)
+
 
 @db_connect
 def query_profile(id, connection, cursor):
-    pass
+    if id is not None:
+        execute_string = build_select('profile', ['id'])
+        cursor.execute(execute_string, (id,))
+    else:
+        execute_string = build_select('profile', [])
+        cursor.execute(execute_string)
+    return create_return_dict(cursor)
 
 @db_connect
 def query_order(id, rid, pid, connection, cursor):
-    pass
+    table_name = "order_parent JOIN order_element ON order_element.oid = order_parent.id"
+    if id is None and rid is None and pid is None:
+        execute_string = build_select(table_name, [])
+        cursor.execute(execute_string)
+    else:
+        n_ = []
+        t_ = []
+        if id is not None:
+            n_.append('id')
+            t_.append(id)
+        if rid is not None:
+            n_.append('rid')
+            t_.append(rid)
+        if pid is not None:
+            n_.append('pid')
+            t_.append(pid)
+        execute_string = build_select(table_name, n_)
+        cursor.execute(execute_string, t_)
+    return create_return_dict(cursor)
 
 @db_connect
-def query_alert(id, rid, status, connection, cursor):
-    pass
+def query_alert(id, rid, pid, status, connection, cursor):
+    if id is None and rid is None and pid is None and status is None:
+        execute_string = build_select('alerts', [])
+        cursor.execute(execute_string)
+    else:
+        n_ = []
+        t_ = []
+        if id is not None:
+            n_.append('id')
+            t_.append(id)
+        if rid is not None:
+            n_.append('rid')
+            t_.append(rid)
+        if pid is not None:
+            n_.append('pid')
+            t_.append(pid)
+        if status is not None:
+            n_.append('status')
+            t_.append(status)
+        execute_string = build_select('alerts', n_)
+        cursor.execute(execute_string, t_)
+    return create_return_dict(cursor)
 
     
 
 if __name__ == "__main__":
-    print(query_restaurant(None))
+    #print(query_restaurant(None))
+    #print(query_menu(None, None))
+    print(query_alert(None, None, 1, None))
